924ab636ae4b6ed31fead5f1535454ccc2cf10a2
NicolayGolovnev <55239819+NicolayGolovnev@users.noreply.github.com>
2022-06-30 12:50:40 +0700

    Первичная инициализация

---
+++ src/main/java/ru/billing/testwork/TestworkApplication.java
@@ -0,0 +1,11 @@
+package ru.billing.testwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestworkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestworkApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/billing/testwork/TestworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.billing.testwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
